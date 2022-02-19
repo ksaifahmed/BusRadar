c565458df5f71d4234424df2784ec55069fbb5d8
ksaifahmed <ksaifahmed@gmail.com>
2022-02-19 22:41:17 +0600

    bus search ui static done

--- app/src/main/java/com/example/busradar/RouteSearchUI.java
+++ app/src/main/java/com/example/busradar/RouteSearchUI.java
@@ -2,7 +2,9 @@ package com.example.busradar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RouteSearchUI extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class RouteSearchUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_search_ui);
+
+        //back button to home
+        TextView back_btn = findViewById(R.id.route_search_back);
+        back_btn.setOnClickListener(view -> go_back_home());
+    }
+
+    //loads home again
+    private void go_back_home() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

7adedfb74fe4757235508a6476250eb98af0859b
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-02-20 09:46:17 +0600

    route search show bus ui

---
+++ app/src/main/java/com/example/busradar/RouteSearchShowBusUI.java
@@ -0,0 +1,16 @@
+package com.example.busradar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RouteSearchShowBusUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_search_show_bus_ui);
+    }
+}

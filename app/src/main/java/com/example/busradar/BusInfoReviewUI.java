c607249eb64373b18d51b6550926eac60d2e42e6
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-02-20 10:59:53 +0600

    bus info review ui

---
+++ app/src/main/java/com/example/busradar/BusInfoReviewUI.java
@@ -0,0 +1,14 @@
+package com.example.busradar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusInfoReviewUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_info_review_ui);
+    }
+}
